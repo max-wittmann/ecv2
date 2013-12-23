3bf45d739d99a689600828968c0128baa3ba5322
Maximilian Wittmann <damxam@gmail.com>
2013-12-24 10:42:56 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MostlyStaticPages::Application.config.secret_key_base = 'ea2db0b82f6f68373652a288c907b7ae8f8218608fda431c7959b7e50341aca1a1b6cb93395b50b43a3d167c07f075cecc2b10514d88b1ffacf2d8959b37b58b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MostlyStaticPages::Application.config.session_store :cookie_store, key: '_mostly_static_pages_session'
