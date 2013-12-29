395082a153b53fde3346d02421e1a7274f2979a0
Maximilian Wittmann <damxam@gmail.com>
2013-12-29 17:58:01 +1100

    Add log in/out functionality and messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 MostlyStaticPages::Application.routes.draw do
-  #get "users/new"
-  #get "static_pages/home"
-  #get "static_pages/help"
-  #get "static_pages/about"
-  #get "static_pages/contact"
+
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
+  match '/signup',  to: 'users#new',        via: 'get'
+  match '/signin',  to: 'sessions#new',     via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup',  to: 'users#new',            via: 'get'
+  # match '/signup',  to: 'users#new',            via: 'get'
   match '/',        to: 'static_pages#home',    via: 'get'
-
 
 
   # The priority is based upon order of creation: first created -> highest priority.
